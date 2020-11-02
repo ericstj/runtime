b08e1e2dfdae189dc4aff27473a34cbe83aa98f8
Eric StJohn <ericstj@microsoft.com>
2020-11-02 12:40:10 -0800

    Represent API changes in BinaryData

--- src/libraries/System.Memory.Data/src/System/BinaryData.cs
+++ src/libraries/System.Memory.Data/src/System/BinaryData.cs
@@ -12,19 +12,20 @@ namespace System
     public class BinaryData
     {
         public BinaryData(byte[] data) { }
-        public BinaryData(object jsonSerializable, JsonSerializerOptions options = default, Type? type = null) { }
+        public BinaryData(object? jsonSerializable, JsonSerializerOptions? options = default, Type? type = null) { }
         public BinaryData(ReadOnlyMemory<byte> data) { }
         public BinaryData(string data) { }
         public static BinaryData FromBytes(ReadOnlyMemory<byte> data) => default(BinaryData);
         public static BinaryData FromBytes(byte[] data) => default(BinaryData);
-        public static BinaryData FromObjectAsJson<T>(T jsonSerializable, JsonSerializerOptions options = default, CancellationToken cancellationToken = default) => default(BinaryData);
+        public static BinaryData FromObjectAsJson<T>(T jsonSerializable, JsonSerializerOptions? options = default) => default(BinaryData);
         public static BinaryData FromStream(Stream stream) => default(BinaryData);
         public static Task<BinaryData> FromStreamAsync(Stream stream, CancellationToken cancellationToken = default) => default(Task<BinaryData>);
         public static BinaryData FromString(string data) => default(BinaryData);
-        public static implicit operator ReadOnlyMemory<byte>(BinaryData data) => default(ReadOnlyMemory<byte>);
-        public static implicit operator ReadOnlySpan<byte>(BinaryData data) => default(ReadOnlySpan<byte>);
-        public ReadOnlyMemory<byte> AsBytes() => default(ReadOnlyMemory<byte>);
-        public T ToObjectFromJson<T>(JsonSerializerOptions options = default, CancellationToken cancellationToken = default) => default(T);
+        public static implicit operator ReadOnlyMemory<byte>(BinaryData? data) => default(ReadOnlyMemory<byte>);
+        public static implicit operator ReadOnlySpan<byte>(BinaryData? data) => default(ReadOnlySpan<byte>);
+        public byte[] ToArray() => default(byte[]);
+        public System.ReadOnlyMemory<byte> ToMemory() => default(ReadOnlyMemory<byte>);
+        public T ToObjectFromJson<T>(JsonSerializerOptions? options = default) => default(T);
         public Stream ToStream() => default(Stream);
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object? obj) => default(bool);
